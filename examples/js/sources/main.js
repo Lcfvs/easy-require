3c6fe82e98505b40096777f1b6d3015cd09fe4f8
Lcf.vs <lcf.vs@hotmail.com>
2014-10-28 21:46:15 +0100

    Update main.js

--- examples/js/sources/main.js
+++ examples/js/sources/main.js
@@ -1,8 +1,18 @@
-require(
-    // module dependencies
-    ['example', 'url'],
-    // module definition
-    function define(example, url) {
-        console.log(example, url);
+require.plugins.push(
+    'global.require(\'logger\', '
+    + function (logger) {
+        logger(__filename);
     }
+    + ');'
 );
+
+require('logger', function () {
+    require(
+        // module dependencies
+        ['example', 'url'],
+        // module definition
+        function define(example, url) {
+            console.log(example, url);
+        }
+    );
+});
